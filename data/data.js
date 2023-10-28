d2194af4ccb48cd9b41ef4c0996f38dbf81dc2da
alunonoite <lucianoccc69@gmail.com>
2023-10-27 21:36:49 -0300

    20232710

---
+++ data/data.js
@@ -0,0 +1,21 @@
+const portifolio = {
+    username: "Luciano CCC",
+    role: "Software Developer",
+    social: ["./assets/imagens/github.svg", "./assets/imagens/email.svg", "./assets/imagens/linkedin.svg"],
+    resume: "Lorem ipsum dolor sit amet consectetur, adipisicing elit"+
+    "Delectus a laudantium fuga nulla recusandae odit tempore" +
+    "earum maxime, dignissimos natus quis quam magni eos quo." +
+    "Est magnam eos officia sint ullam, blanditiis, nobis," +
+    "laboriosam obcaecati excepturi non sunt rem! Omnis, quos" +
+    "assumenda? Laudantium harum officia laboriosam odio sed" +
+    "deserunt similique?",
+    projects: [
+        {name:"", tools: [], url: ""},
+        {name:"", tools: [], url: ""},
+        {name:"", tools: [], url: ""},
+        {name:"", tools: [], url: ""},
+        {name:"", tools: [], url: ""},
+    ]
+}
+
+export {portifolio}
